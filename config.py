4cdd7fdc5798ce6e75c56176665de4c727f7e08c
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2017-08-10 15:30:03 +0000

    Update config

--- config.py
+++ config.py
@@ -3,15 +3,15 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--gpu', default=0)
-    parser.add_argument('--max-iter', default=10000)
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--max-iter', default=100000)
     parser.add_argument('--batch-size', default=128)
     parser.add_argument('--out', default='result_2')
 
     # Snapshot intervals
     parser.add_argument('--iter-log', default=1)
     parser.add_argument('--iter-plot', default=1)
-    parser.add_argument('--iter-model-snapshot', default=1000)
+    parser.add_argument('--iter-model-snapshot', default=10000)
 
     # Test specific
     parser.add_argument('--test-model', default='model_1000')
